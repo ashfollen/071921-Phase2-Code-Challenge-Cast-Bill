61a8edf556f9c469b4a61f15e9b9ea4ca8e77e3a
Ashley Follen <ashleyfollen@outlook.com>
2021-08-20 11:12:39 -0600

    core deliverables complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BillCollection from './components/BillCollection';
 import BillsCast from './components/BillsCast';
 
+
 export default function App() {
-  //start here with your code for step one
+  const [ bills, setBills ] = useState([])
+  console.log(bills)
+  
+  useEffect(() => {
+    fetch('http://localhost:8002/bills')
+    .then(resp => resp.json())
+    .then(data => setBills(data))
+  }, [])
+
+  function addToCast(id) {
+    console.log(id);
+    setBills(
+      bills.map((bill) => bill.id === id ? {...bill, cast: true} : bill)
+    )
+  }
+
+  function removeFromCast(id) {
+    setBills(
+      bills.map((bill) => bill.id === id ? {...bill, cast: false} : bill)
+    )
+  }
+
+  function fireBill(id) {
+    setBills(
+      bills.filter((bill) => bill.id !== id)
+    )
+  }
 
   return (
     <div>
-      <BillsCast />
-      <BillCollection />
+      <BillsCast bills={bills.filter(bill => bill.cast)} handleClick={removeFromCast} fireBill={fireBill} />
+      <BillCollection bills={ bills } handleClick={addToCast} fireBill={fireBill} />
     </div>
   );
 }
+
+// Component  Hierarchy
+// 
+// App 
+// -- BillsCast
+// ---- BillCard
+// -- BillCollection
+// ---- BillCard
+
